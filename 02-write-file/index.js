7f75dd559d383fbadce88a0ecf7f2e50166d7079
Jafr0 <zhek42@yandex.ru>
2022-05-24 17:53:06 +0300

    feat: add hello message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,12 +2,20 @@ let fs = require("fs");
 const path = require('path');
 const { stdin, stdout, exit } = process;
 let file = path.join(__dirname, 'output.txt');
+const readline = require('readline');
 
 const output = fs.createWriteStream(file);
 
+let message = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: 'Можешь начинать!))0)\n'
+});
 
+message.prompt();
 
 stdin.on('data', function (chunk) {
+
 	let str = chunk.toString()
 
 
